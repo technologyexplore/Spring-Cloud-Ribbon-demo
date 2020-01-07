e238a06055ff414c27c9e3592280fc388ca12e0b
zhuyongbiao <534109494@qq.com>
2020-01-07 11:44:43 +0800

    spring cloud ribbon

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Spring-Cloud-Ribbon-demo
 Spring Cloud Ribbon 理论与实践
+
+      1.Spring Cloud Ribbon 介绍
+      2.Spring Cloud Ribbon 与 nginx的对比
+      3.Spring Cloud Ribbon 使用
+
---
+++ yanshi/microservice-provider-user3/src/main/java/com/itmuch/cloud/study/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.itmuch.cloud.study.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itmuch.cloud.study.entity.User;
+import com.itmuch.cloud.study.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/{id}")
+	public User findById(@PathVariable Long id) {
+		LOG.warn("服务3 findById接口");
+		User findOne = this.userRepository.findOne(id);
+		return findOne;
+	}
+}
